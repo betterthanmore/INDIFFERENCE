0b39e76cfbd4c0332ebf89c80f3e6d35dfd07759
HS010315 <hspower01@gmail.com>
2024-10-14 08:37:52 +0900

    [Feat] 플레이어를 따라다니는 카메라 추가

---
+++ INDIFFERENCE_2024/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform target;
+    Vector3 velocity = Vector3.zero;
+
+    [Range(0, 1)]
+    public float smoothTime;
+
+    public Vector3 positionOffest;
+    public Vector2 xLimit;
+    public Vector2 yLimit;
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position+positionOffest;
+        targetPosition = new Vector3(Mathf.Clamp(targetPosition.x, xLimit.x, xLimit.y), Mathf.Clamp(targetPosition.y, yLimit.x, yLimit.y), -10);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
