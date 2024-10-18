8e4149da3e770420e484d3ee0c915451b7d7c97e
hojoon018 <skewed018@gmail.com>
2024-10-18 16:52:56 +0900

    [Feat] 환경설정 UI 구현
    추가 작업 및 수정 예정

---
+++ INDIFFERENCE_2024/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    public GameObject settingMenu;
+
+    public bool isPaused = false;
+
+    void Start()
+    {
+        settingMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(!isPaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        settingMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        settingMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnResumeButton()
+    {
+        Resume();
+    }
+}
